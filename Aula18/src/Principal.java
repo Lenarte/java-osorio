656518ad879ce45e321bdd7e6a927a910254cf85
Lenarte <paulohenriquecof@gmail.com>
2024-04-26 11:25:55 -0300

    testes executados na classe principal

--- Aula18/src/Principal.java
+++ Aula18/src/Principal.java
@@ -2,6 +2,16 @@ public class Principal {
 
     public static void main(String[] args) {
         
+        Heroi heroi1 = new Heroi("Paulo, o grande", 30, "Barbaro");
+
+        Vilao vilao1 = new Vilao();
+        Vilao vilao2 = new Vilao("Skull", 20, 2);
+
+        System.out.println("Heroi 1:\n" + heroi1);
+        System.out.println("Vilão1:\n" + vilao1);
+        System.out.println("Vilão2:\n" + vilao2);
+
+
         
     }
     
--- Aula18/src/Vilao.java
+++ Aula18/src/Vilao.java
@@ -4,11 +4,16 @@ public class Vilao extends Personagem {
 
     public Vilao() {
 
+        setNome("Slime");
+        setPtsVida(1);
+        nivel = 1;
+
     }
 
     public Vilao(String nome, int ptsVida, int nivel) {
         super(nome, ptsVida);
         this.nivel = nivel;
+        setPtsVida(ptsVida * nivel);
     }
 
     public int getNivel() {
