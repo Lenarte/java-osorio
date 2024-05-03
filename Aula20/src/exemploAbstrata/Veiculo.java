fae757eb91b4900a990ba39cc29d34771ef25ab7
Lenarte <paulohenriquecof@gmail.com>
2024-05-03 10:26:34 -0300

    commit teste

---
+++ Aula20/src/exemploAbstrata/Veiculo.java
@@ -0,0 +1,35 @@
+package exemploAbstrata;
+
+public abstract class Veiculo {
+
+    private String placa;
+
+    public Veiculo() {
+
+    }
+
+    public Veiculo(String placa) {
+        this.placa = placa;
+    }
+    
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    @Override
+    public String toString() {
+
+        return "\nPlaca: " + placa;
+    }
+
+    //metodo abstrato: escrevemos apenas suas assinaturas
+
+    public abstract String acelerar();
+
+
+
+}
